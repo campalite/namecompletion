7003e3bd9413927738cf1df41877d490ced30d0e
campalite <ysfturker97@gmail.com>
2019-03-12 14:37:41 +0200

    Migration

---
+++ database/migrations/2019_03_12_122317_create_person_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->String('name');
+			$table->String('surname');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person');
+    }
+}
